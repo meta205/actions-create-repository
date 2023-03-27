6c4efd58880745256a7ae24df07752275ff232cb
Kyongho Lee <95116876+meta205@users.noreply.github.com>
2023-03-27 10:17:47 +0900

    Init commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# GitHub Action for creating a GitHub Repository
---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import * as core from '@actions/core';
+import { Octokit } from '@octokit/rest';
+
+(async (): Promise<void> => {
+  try {
+    const octokit = new Octokit({
+      auth: process.env.GITHUB_TOKEN
+    });
+
+    const name: string = core.getInput('name');
+
+    await octokit.rest.repos.createForAuthenticatedUser({
+      name,
+    });
+  } catch (err: any) {
+    core.setFailed(err.message);
+  }
+})();
